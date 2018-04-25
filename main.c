1d8d4f66f246ecda3d6359a84adebf36eb833950
JaeSeung Kim <shine8917@gmail.com>
2018-04-24 20:42:45 -0700

    shine8917

--- main.c
+++ main.c
@@ -55,11 +55,29 @@ void filetime2(void)
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(void)
 {
+	int s1 = (int)stat1.st_size; //st_size를 사용하여 stat1의 size를 s1에 저장한다.
+	int s2 = (int)stat2.st_size; //st_size를 사용하여 stat2의 size를 s2에 저장한다.
+	printf("size : ");
+	if(s1 > s2) //s1이 s2보다 크다면 조건문 실행
+		printf("text1 is bigger\n"); //text1 is bigger 출력
+	else if(s1 < s2) //s2가 s1보다 크다면 조건문 실행
+		printf("text2 is bigger\n"); //text2 is bigger 출력
+	else //s2와 s1이 같다면 조건문 실행
+		printf("sizes are equal\n"); //sizes are equal 출력
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(void)
 {
+	int s1 = (int)stat1.st_blocks; //st_size를 사용하여 stat1의 blocks를 s1에 저장한다.
+	int s2 = (int)stat2.st_blocks; //st_size를 사용하여 stat2의 blocks를 s1에 저장한다.
+	printf("blocks : ");
+	if(s1 > s2) //s1이 s2보다 크다면 조건문 실행
+			printf("text1 is bigger\n"); //text1 is bigger 출력
+		else if(s1 < s2) //s2가 s1보다 크다면 조건문 실행
+			printf("text2 is bigger\n"); //text2 is bigger 출력
+		else //s2와 s1이 같다면 조건문 실행
+			printf("block sizes are equal\n"); //sizes are equal 출력
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
